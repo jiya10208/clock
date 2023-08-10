0d6cb3701e2433e99cc7c829bbe95c81af560ad2
jiya10208 <jiya102082004@gmail.com>
2023-08-10 18:10:20 +0530

    clock

---
+++ script.js
@@ -0,0 +1,22 @@
+setInterval(setClock, 1000)
+
+const hourHand=document.querySelector('[data-hour-hand]')
+const minuteHand=document.querySelector('[data-minute-hand]')
+const secondHand=document.querySelector('[data-second-hand]')
+
+function setClock(){
+	const currentDate=new Date()
+	const secondRatio=currentDate.getSeconds()/60
+	const minutesRatio=(secondRatio+currentDate.getMinutes())/60
+	const hourRatio=(minutesRatio+currentDate.getHours())/60
+
+	setrotation(secondHand, secondRatio)
+	setrotation(minuteHand, minutesRatio)
+	setrotation(hourHand, hourRatio)
+}
+
+function setrotation(element, rotationRatio){
+	element.style.setProperty('--rotation', rotationRatio*360)
+}
+
+setClock()
